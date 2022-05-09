629f63951ddde1c663b920bf867a9152ad755dd2
Roddy <roddyfcks@gmail.com>
2022-05-09 09:41:44 +0100

    fix broken icons in marketplace link buttons in hero

--- src/components/heroblock.js
+++ src/components/heroblock.js
@@ -75,11 +75,7 @@ const HeroBlock = () => {
         rel="noopener noreferrer"
       >
         MagicEden
-        <LinkIcon
-          fill="currentColor"
-          class="bi bi-box-arrow-up-right"
-          viewBox="0 0 16 16"
-        >
+        <LinkIcon fill="currentColor" viewBox="0 0 16 16">
           <path
             fill-rule="evenodd"
             d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
@@ -96,11 +92,7 @@ const HeroBlock = () => {
         rel="noopener noreferrer"
       >
         OpenSea
-        <LinkIcon
-          fill="currentColor"
-          class="bi bi-box-arrow-up-right"
-          viewBox="0 0 16 16"
-        >
+        <LinkIcon fill="currentColor" viewBox="0 0 16 16">
           <path
             fill-rule="evenodd"
             d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5z"
